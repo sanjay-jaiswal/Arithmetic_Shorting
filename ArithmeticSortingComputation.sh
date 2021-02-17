5f456971c56cfba9093683ba346cae1249a32dc4
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-02-17 08:15:27 +0530

    Added: Sort in Descending order

--- ArithmeticSortingComputation.sh
+++ ArithmeticSortingComputation.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 echo "---Arithmetic sorting---"
+
+
+
+function descendingOrder(){
+   for (( i=1;i<=4;i++ ))
+   do
+      for (( j=1;j<=4;j++ ))
+      do
+         if [ ${array[$i]} -gt ${array[$j]} ]
+         then
+            Temp=${array[$i]};
+            array[$i]=${array[$j]};
+            array[$j]=$Temp;
+         fi
+      done
+   done
+}
